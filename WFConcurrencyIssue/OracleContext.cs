cbd3f6b329296d962f7c64b8a5fca50f8d122510
inferno31 <40772469+inferno31@users.noreply.github.com>
2019-10-25 09:48:58 -0400

    Upload sample

---
+++ WFConcurrencyIssue/OracleContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Logging;
+using WorkflowCore.Persistence.EntityFramework.Models;
+using WorkflowCore.Persistence.EntityFramework.Services;
+
+namespace WorkflowCore.Persistence.Oracle
+{
+    public class OracleContext : WorkflowDbContext
+    {
+        private readonly string _connectionString;
+
+        public OracleContext(DbContextOptions<OracleContext> options)
+            : base(options)
+        { }
+
+        protected override void ConfigureSubscriptionStorage(EntityTypeBuilder<PersistedSubscription> builder)
+        {
+            builder.ToTable("Subscription", "WFC");
+            builder.Property(x => x.PersistenceId).UseOracleIdentityColumn();
+        }
+
+        protected override void ConfigureWorkflowStorage(EntityTypeBuilder<PersistedWorkflow> builder)
+        {
+            builder.ToTable("Workflow", "WFC");
+            builder.Property(x => x.PersistenceId).UseOracleIdentityColumn();
+        }
+
+        protected override void ConfigureExecutionPointerStorage(EntityTypeBuilder<PersistedExecutionPointer> builder)
+        {
+            builder.ToTable("ExecutionPointer", "WFC");
+            builder.Property(x => x.PersistenceId).UseOracleIdentityColumn();
+        }
+
+        protected override void ConfigureExecutionErrorStorage(EntityTypeBuilder<PersistedExecutionError> builder)
+        {
+            builder.ToTable("ExecutionError", "WFC");
+            builder.Property(x => x.PersistenceId).UseOracleIdentityColumn();
+        }
+
+        protected override void ConfigureExetensionAttributeStorage(EntityTypeBuilder<PersistedExtensionAttribute> builder)
+        {
+            builder.ToTable("ExtensionAttribute", "WFC");
+            builder.Property(x => x.PersistenceId).UseOracleIdentityColumn();
+        }
+
+        protected override void ConfigureEventStorage(EntityTypeBuilder<PersistedEvent> builder)
+        {
+            builder.ToTable("Event", "WFC");
+            builder.Property(x => x.PersistenceId).UseOracleIdentityColumn();
+        }
+    }
+}
---
+++ WFConcurrencyIssue/Program.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkflowCore.Persistence.EntityFramework.Models;
+using WorkflowCore.Persistence.Oracle;
+
+namespace WFConcurrencyIssue
+{
+    class Program
+    {
+        static string connectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.56.102)(PORT=32118)))" +
+            "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));User Id=WFC;Password=Oracle18;";
+
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = BuildServiceProvider();
+
+            long wfId;
+
+            // setup
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<OracleContext>();
+
+                context.Database.EnsureCreated();
+
+                var wf = new PersistedWorkflow()
+                {
+                    CreateTime = DateTime.Now,
+                    Data = "{\"$type\":\"WorkflowCore.Tests.Oracle.Scenarios.SimpleEventScenario + MyDataClass, WorkflowCore.Tests.Oracle\",\"StrValue1\":\"1b98c27f-f33c-48b3-bffa-a9f758f1e74c\",\"StrValue2\":\"0be48870-6741-467a-a3e1-f7becfc6e681\"}",
+                    InstanceId = Guid.NewGuid(),
+                    NextExecution = 0,
+                    Status = 0,
+                    Version = 1,
+                    WorkflowDefinitionId = "EventWorkflow"
+                };
+
+                var predecessor = new PersistedExecutionPointer()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now,
+                    StepId = 0,
+                    Active = false,
+                    Status = PointerStatus.Complete
+                };
+                wf.ExecutionPointers.Add(predecessor);
+
+                wf.ExecutionPointers.Add(new PersistedExecutionPointer()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Active = true,
+                    StartTime = DateTime.Now,
+                    EventKey = "1b98c27f-f33c-48b3-bffa-a9f758f1e74c",
+                    EventName = "MyEvent",
+                    EventPublished = true,
+                    PredecessorId = predecessor.Id,
+                    StepId = 1,
+                    Status = PointerStatus.WaitingForEvent
+                });
+
+                context.Set<PersistedWorkflow>().Add(wf);
+                await context.SaveChangesAsync();
+
+                wfId = wf.PersistenceId;
+            }
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<OracleContext>();
+
+                var wf = await context.Set<PersistedWorkflow>()
+                    .Include(x => x.ExecutionPointers)
+                    .FirstAsync(w => w.PersistenceId == wfId);
+
+                wf.Data = "{\"$type\":\"WorkflowCore.Tests.Oracle.Scenarios.SimpleEventScenario+MyDataClass, WorkflowCore.Tests.Oracle\",\"StrValue1\":\"1b98c27f-f33c-48b3-bffa-a9f758f1e74c\",\"StrValue2\":\"0be48870-6741-467a-a3e1-f7becfc6e681\"}";
+                
+                var step0 = wf.ExecutionPointers.Single(e => e.StepId == 0);
+                step0.Children = string.Empty;
+                step0.Scope = string.Empty;
+
+                var step1 = wf.ExecutionPointers.Single(e => e.StepId == 1);
+                step1.Active = false;
+                step1.Children = string.Empty;
+                step1.Scope = string.Empty;
+                step1.EndTime = DateTime.Now;
+
+                wf.ExecutionPointers.Add(new PersistedExecutionPointer()
+                {
+                    Active = true,
+                    Children = string.Empty,
+                    ContextItem = "null",
+                    EndTime = null,
+                    EventData = "null",
+                    EventKey = string.Empty,
+                    EventName = string.Empty,
+                    EventPublished = false,
+                    Id = Guid.NewGuid().ToString(),
+                    Outcome = "null",
+                    PersistenceData = "null",
+                    PredecessorId = step1.Id,
+                    RetryCount = 0,
+                    Scope = string.Empty,
+                    SleepUntil = null,
+                    StartTime = DateTime.Now,
+                    Status = PointerStatus.Pending,
+                    StepId = 2,
+                    StepName = null,
+                });
+                
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private static IServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(l =>
+            {
+                l.AddDebug();
+                l.AddConsole();
+            });
+
+            services.AddDbContext<OracleContext>(o =>
+            {
+                o.UseOracle(connectionString);
+            });
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
